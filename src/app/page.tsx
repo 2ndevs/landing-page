8c89a417ae9e23907c94bdc80749acadef567683
Nicolas <89911360+d-kja@users.noreply.github.com>
2024-08-18 19:04:24 -0300

    Feat/navbar (#1)
    * dawodkawo
    
    * x
    
    * feat adjwioadja
    
    * awjdiawjdi
    
    * awjdiawjdi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
+import { Navbar } from "@/components/shards/navbar";
 import { Hero } from "@components/shards/hero";
 
 const Home = () => {
-  return (
-    <div className="max-w-screen-lg">
-      {/* # Navigation */}
+  return <div className="max-w-screen-lg">
+    {/* # Navigation */}
+    <Navbar />
 
-      {/* # Body */}
-      <Hero />
+    {/* # Body */}
+    <Hero />
 
-      {/* # Footer */}
-    </div>
-  );
+    {/* # Footer */}
+  </div>
 };
 
 export default Home;
