8d724ec0e7775c7904034f7afca12010273b9ca3
ndelanhese <nnathanh3@gmail.com>
2024-08-25 17:29:38 -0300

    feat: add our services section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,20 @@
-import { Navbar } from "@/components/shards/navbar";
+import { Navbar } from "@components/shards/navbar";
 import { Hero } from "@components/shards/hero";
+import { OurServices } from "@components/shards/our-services";
 
 const Home = () => {
-  return <div className="max-w-screen-lg">
-    {/* # Navigation */}
-    <Navbar />
+  return (
+    <div className="max-w-screen-lg">
+      {/* # Navigation */}
+      <Navbar />
 
-    {/* # Body */}
-    <Hero />
+      {/* # Body */}
+      <Hero />
+      <OurServices />
 
-    {/* # Footer */}
-  </div>
+      {/* # Footer */}
+    </div>
+  );
 };
 
 export default Home;
