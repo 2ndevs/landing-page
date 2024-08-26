4bc145da0392ed0f13113308ec42ccef0d8170d2
ndelanhese <nnathanh3@gmail.com>
2024-08-26 08:22:54 -0300

    fix: mobile view

--- src/components/shards/show-case-card.tsx
+++ src/components/shards/show-case-card.tsx
@@ -5,15 +5,17 @@ import Image from "next/image";
 export const ShowCaseCard = () => {
 	const imageURL = new URL("/hero-dark.png", env.NEXT_PUBLIC_APP_URL);
 	return (
-		<div className="relative h-[31.25rem] w-full overflow-hidden rounded-lg mt-12 leading-none image-gradient">
-			<Image
-				className="object-cover"
-				alt="Show case card image"
-				src={imageURL.toString()}
-				height={500}
-				width={1024}
-			/>
-			<BorderBeam size={250} duration={12} delay={9} />
-		</div>
+		<section className="sm:h-[31.25rem] w-full overflow-hidden sm:p-0 p-4 sm:mt-12 mt-6">
+			<div className="relative rounded-lg image-gradient">
+				<Image
+					className="object-cover"
+					alt="Show case card image"
+					src={imageURL.toString()}
+					height={500}
+					width={1024}
+				/>
+				<BorderBeam size={250} duration={12} delay={9} />
+			</div>
+		</section>
 	);
 };
